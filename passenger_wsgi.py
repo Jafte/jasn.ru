516473fbb5f16b6ecb0cdf071d4adcbf0e8cd29b
Aleksandr Pyatnitsin <support@beget.ru>
2016-10-10 20:47:44 +0300

    init

---
+++ passenger_wsgi.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+import os
+import sys
+
+BASE_APP_DIR = os.path.dirname(os.path.abspath(__file__))
+
+sys.path.append(os.path.join(BASE_APP_DIR, 'src/'))
+sys.path.append(os.path.join(BASE_APP_DIR, 'venv/lib/python3.4/site-packages'))
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app.settings")
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
