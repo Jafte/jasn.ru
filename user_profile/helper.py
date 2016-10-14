8f6a1e154daa497b0955bc0643af330955150d3e
jafte <jafte@ya.ru>
2016-10-14 04:26:03 +0300

    fix name

--- user_profile/helper.py
+++ user_profile/helper.py
@@ -1,10 +1,10 @@
 def get_full_name_or_username(user):
     result = []
     if user.first_name or user.last_name:
-        if user.first_name:
-            result.append(user.first_name)
         if user.last_name:
             result.append(user.last_name)
+        if user.first_name:
+            result.append(user.first_name)
     else:
         result.append(user.username)
 
