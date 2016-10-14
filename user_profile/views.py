d8f99c187b184fdab605a6b55cacbaebd3001a84
jafte <jafte@ya.ru>
2016-10-14 03:56:13 +0300

    fixes

--- user_profile/views.py
+++ user_profile/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 
 class UserDetail(DetailView):
     slug_field = 'username'
+    context_object_name = "user_data"
     slug_url_kwarg = 'username'
     queryset = User.objects.filter(is_active=True)
     template_name = 'user_profile/user_detail.html'
