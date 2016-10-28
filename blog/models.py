ec1f16bfec6b6bc8b7016eb47a16ac41787df984
jafte <jafte@ya.ru>
2016-10-28 20:05:53 +0300

    blog

--- blog/models.py
+++ blog/models.py
@@ -10,6 +10,21 @@ from django.db.models.signals import post_save
 
 import datetime
 
+STATUS_CHOICES = (
+    (1, _('Draft')),
+    (2, _('Public')),
+)
+
+TYPE_CHOICES = (
+    (1, _('Text')),
+    (2, _('Quotation')),
+    (3, _('Link')),
+    (4, _('Video')),
+    (5, _('Single Image')),
+    (6, _('Gallery Image')),
+)
+
+
 @python_2_unicode_compatible
 class Blog(models.Model):
     title = models.CharField(_('title'), max_length=100)
@@ -31,6 +46,9 @@ class Blog(models.Model):
     def get_absolute_url(self):
         return reverse('blog-detail', kwargs={'blog_slug': self.slug})
 
+    def get_posts(self):
+        return self.posts.all().filter(blog=self, active=True, published__lte=datetime.datetime.now())
+
 
 def blog_changed(sender, **kwargs):
     blog = kwargs["instance"]
@@ -42,10 +60,6 @@ post_save.connect(blog_changed, sender=Blog)
 
 @python_2_unicode_compatible
 class Post(models.Model):
-    STATUS_CHOICES = (
-        (1, _('Draft')),
-        (2, _('Public')),
-    )
     title = models.CharField(_('title'), max_length=200)
     author = models.ForeignKey(User)
     blog = models.ForeignKey(Blog, related_name='posts')
@@ -58,7 +72,7 @@ class Post(models.Model):
     class Meta:
         verbose_name = _('post')
         verbose_name_plural = _('posts')
-        ordering  = ('-published',)
+        ordering = ('-published',)
         get_latest_by = 'published'
 
     def __str__(self):
@@ -66,3 +80,32 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse('blog-post-detail', kwargs={'blog_slug': self.blog.slug, 'post_pk': self.pk})
+
+
+@python_2_unicode_compatible
+class PostBlock(models.Model):
+    post = models.ForeignKey(Post, related_name='blocks')
+    type = models.IntegerField(_('type'), choices=TYPE_CHOICES, default=1)
+    text = models.TextField(_('data'))
+    description = models.CharField(_('description'), max_length=200, blank=True)
+    owner = models.ForeignKey(User, related_name='blocks')
+    order = models.IntegerField(_('order'), default=100)
+    created = models.DateTimeField(_('created'), auto_now_add=True)
+    modified = models.DateTimeField(_('modified'), auto_now=True)
+
+    def __str__(self):
+        return 'block %s for %s' % (self.type, self.post)
+
+
+@python_2_unicode_compatible
+class PostImage(models.Model):
+    post = models.ForeignKey(Post, related_name='images')
+    block = models.ManyToManyField(PostBlock, related_name='images', blank=True, null=True)
+    owner = models.ForeignKey(User, related_name='images')
+    image = ThumbnailerImageField(upload_to=upload_to)
+    description = models.CharField(_('description'), max_length=200, blank=True)
+    created = models.DateTimeField(_('created'), auto_now_add=True)
+    modified = models.DateTimeField(_('modified'), auto_now=True)
+
+    def __str__(self):
+        return 'image for %s' % self.post
